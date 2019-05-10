4c5c4c54f370c035c932ad4f171d70e9f8767c56
eozmen410 <eo2419@columbia.edu>
2019-05-10 02:20:01 -0400

    changed buttons for instructions

--- static/coffee.js
+++ static/coffee.js
@@ -75,6 +75,7 @@ $(document).ready(function(){
         tm = min_left + (seconds_left/60)
         $("#timer_start").attr('disabled',false)
         $("#timer_stop").attr('disabled',true)
+        $("#timer_restart").attr('disabled',false)
     })
 
     $("#timer_restart").click(function(){
@@ -101,46 +102,6 @@ $(document).ready(function(){
         updateTimer()
         tm = brew['timer'][step]
     })
-
-  //    /* 1. Visualizing things on Hover - See next part for action on click */
-  // $('#stars li').on('mouseover', function(){
-  //   var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
-   
-  //   // Now highlight all the stars that's not after the current hovered star
-  //   $(this).parent().children('li.star').each(function(e){
-  //     if (e < onStar) {
-  //       $(this).addClass('hover');
-  //     }
-  //     else {
-  //       $(this).removeClass('hover');
-  //     }
-  //   });
-    
-  // }).on('mouseout', function(){
-  //   $(this).parent().children('li.star').each(function(e){
-  //     $(this).removeClass('hover');
-  //   });
-  // });
-  
-  
-  // /* 2. Action to perform on click */
-  // $('#stars li').on('click', function(){
-  //   var onStar = parseInt($(this).data('value'), 10); // The star currently selected
-  //   var stars = $(this).parent().children('li.star');
-    
-  //   for (i = 0; i < stars.length; i++) {
-  //     $(stars[i]).removeClass('selected');
-  //   }
-    
-  //   for (i = 0; i < onStar; i++) {
-  //     $(stars[i]).addClass('selected');
-  //   }
-    
-  //   // JUST RESPONSE (Not needed)
-  //   var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
-  //   saveRating(ratingValue)
-  // });
-
 
 })
 
